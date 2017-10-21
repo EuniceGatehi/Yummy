5b1363be4397a8dcddfa0d0de28429df611357a3
Eunice Gatehi <eunicegatehi@gmail.com>
2017-10-21 13:05:19 +0300

    update user.py and other static files

--- user.py
+++ user.py
@@ -3,65 +3,7 @@ import re
 users = {}
 
 
-class User(object):
+class User():
     """
     Class to handle  user functions
     """
-
-    def __init__(self, name=None, email=None, password=None):
-        """ Initializing  class instance variables"""
-        self.name = name
-        self.email = email
-        self.password = password
-
-    def register(self, email, name, password, cpassword):
-        """defining method to create account"""
-        if re.match("[a-zA-Z0-9- .]+$",name):
-            if name != '' and email != '' and password != '':
-                if name not in users.keys():
-                    if email not in users.keys():
-                        if password == cpassword:
-                            regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-                            regPass = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-                            result = email
-                            if re.search(regex, result):
-                                if re.search(regPass, password):
-                                    users[email] = {
-                                        'name': name,
-                                        'email': email,
-                                        'pass': password,
-                                    }
-                                    return 1
-                                return 7
-
-                            return 2
-                        return 3
-                    return 4
-                return 5
-            return 6
-        return 8
-
-
-    def login(self, email, password):
-        """ defining method to Log in user"""
-        if email != '' and password != '':
-            if email in users.keys():
-                result = users[email]
-                pword = result['pass']
-                if pword == password:
-                    return 1
-                return 2
-            return 3
-        return 4
-
-    def get_user_name(self, email):
-        if email in users.keys():
-            result = users[email]
-            return result['name']
-        return False
-
-    def get_user_email(self, email):
-        if email in users.keys():
-            result = users[email]
-            return result['email']
-        return False
